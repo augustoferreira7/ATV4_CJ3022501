1c3c809906ac4cb8e989608a93e4ee3b7f831f58
augustoferreira7 <augusto.enzo@aluno.ifsp.edu.br>
2024-03-20 08:25:21 -0300

    Adicionar arquivos de projeto.

---
+++ ATV4_CJ3022501/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ATV4_CJ3022501
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, n2, operacao;
+            Console.WriteLine("Digite um número:");
+            n = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("escolha uma das opções abaixo");
+            Console.WriteLine("1 = adição\n2 = subtração\n3 = multiplicação\n4 = divisão");
+            operacao = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            switch (operacao)
+            {
+                case 1:
+                    Console.WriteLine(n + n2);
+                    break;
+
+                case 2:
+                    Console.WriteLine(n - n2);
+                    break;
+                case 3:
+                    Console.WriteLine(n * n2);
+                    break;
+                case 4:
+                    Console.WriteLine(n / n2);
+                    break;
+
+                default:
+                    Console.WriteLine("opção inválida");
+                    break;
+            }
+        }
+    }
+
+
+}
